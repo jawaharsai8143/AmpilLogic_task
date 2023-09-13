4e6b6b228037f2b6e298ed874045fd07f97f8f6d
jawaharsai <shaik.jawaharsai12@gmail.com>
2023-09-13 10:47:09 +0530

    AmpilLogic

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  buttonDisabled: boolean | undefined;
+  disableProgressbar: boolean | undefined;
+  disableSuccessMsg: boolean | undefined;
+
+  ngOnInit() {
+    this.buttonDisabled = true;
+    this.disableProgressbar = false;
+    this.disableSuccessMsg = false;
+  }
+
+  startProgress() {
+    this.disableProgressbar = true;
+    this.startProgressBar();
+  }
+
+  progress = 0;
+  showSuccessPopup = false;
+
+  startProgressBar() {
+    this.progress = 0;
+    this.showSuccessPopup = false;
+    this.buttonDisabled = false;
+    
+    const interval = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress += 20; // Adjust the increment as needed
+      } else {
+        clearInterval(interval);
+        this.showSuccessPopup = true;
+        this.closeSuccessPopup();
+      }
+    }, 500); // Adjust the interval as needed
+  }
+
+  closeSuccessPopup() {
+    this.disableProgressbar = false;
+    this.buttonDisabled = false;
+    this.disableSuccessMsg = true;
+  }
+
+}
